8bc1d2052fbc7e051e4bc19b1721f49c8dfd9f2f
Jhulces <jcochoae@gmail.com>
2023-07-10 00:45:43 -0500

    Exercise 1.4 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,38 +1,29 @@
 const Header = ({course}) => (<h1>{course}</h1>);
-
-const Part = ({part}) => (<p>{part.name} {part.exercises}</p>);
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      <Part part={parts[0]}/>
-      <Part part={parts[1]}/>
-      <Part part={parts[2]}/>
-    </div>
-  )
-}
-
+const Content = ({parts}) => (<div>{parts.map((item, idx) => (<p key={idx}>{item.name} {item.exercises}</p>))}</div>);
 const Total = ({parts}) => (<p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>);
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+    {
       name: 'Fundamentals of React',
       exercises: 10
-    }
-  const part2 = {
+    },
+    {
       name: 'Using props to pass data',
       exercises: 7
-    }
-  const part3 = {
+    },
+    {
       name: 'State of a component',
       exercises: 14
     }
+  ]
+
   return (
     <div>
       <Header course={course}/>
-      <Content parts={[part1, part2, part3]}/>
-      <Total parts={[part1, part2, part3]}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
